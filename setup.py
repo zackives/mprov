42226056ab1a70a9053a0f495fb879ba409852be
John Frommeyer <johnfr@seas.upenn.edu>
2018-01-17 12:29:16 -0500

    testing build.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "pennprov"
-VERSION = "1.0.0rc3"
+VERSION = "1.0.0rc5"
 # To install the library, run the following
 #
 # python setup.py install
