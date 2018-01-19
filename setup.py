c96335ee0040fbff989bda125f8ba333de6638d3
John Frommeyer <johnfr@seas.upenn.edu>
2018-01-19 16:20:19 -0500

    Update for version 1.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "pennprov"
-VERSION = "1.0.0rc6"
+VERSION = "1.0.0"
 # To install the library, run the following
 #
 # python setup.py install
