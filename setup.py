e1ed6f2ab66e9583bf7d51cba77325c7358fbf9b
John Frommeyer <johnfr@seas.upenn.edu>
2019-01-24 15:10:50 -0500

    Update from swagger generated code.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "pennprov"
-VERSION = "2.2.0dev3"
+VERSION = "2.2.0.dev4"
 # To install the library, run the following
 #
 # python setup.py install
