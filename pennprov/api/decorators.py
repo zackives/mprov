969dd5bb2188697cfaf6167b0d790fb927f0c133
Zack Ives <zives@seas.upenn.edu>
2020-02-23 16:13:47 -0500

    Fixes for compatibility with Docker and CCortex

--- pennprov/api/decorators.py
+++ pennprov/api/decorators.py
@@ -7,18 +7,20 @@ import os
 from pyspark import broadcast
 from pennprov.connection.mprov_connection import MProvConnection
 from pennprov.metadata.stream_metadata import BasicSchema, BasicTuple
+import pandas as pd
 
 # Try environment variables first
 try:
     mprov_user = os.environ['MPROV_USER']
     mprov_password = os.environ['MPROV_PASSWORD']
-    mprov_conn = MProvConnection(mprov_user, mprov_password, None)
+    mprov_host = os.environ['MPROV_HOST']
+    mprov_conn = None
 except KeyError:
     # Try simple defaults second
     try:
         mprov_user = 'YOUR_USERNAME'
         mprov_password = 'YOUR_PASSWORD'
-        mprov_conn = MProvConnection(mprov_user, mprov_password, None)
+        mprov_conn = None
     except KeyError:
         mprov_conn = None
 
@@ -42,6 +44,8 @@ def MProvAgg(in_stream_name,op,out_stream_name,in_stream_key=['index'],out_strea
     :return:
     """
     def md_key(stream, key_list):
+        if isinstance(stream, tuple) and len(stream) == 1:
+            stream = stream[0]
         # If the stream is a dataframe, return the subset matching the key
         if isinstance(stream, pd.DataFrame):
             if not in_stream_key:
@@ -52,6 +56,8 @@ def MProvAgg(in_stream_name,op,out_stream_name,in_stream_key=['index'],out_strea
             return repr(stream)
 
     def rel_keys(stream, key_list):
+        if isinstance(stream, tuple) and len(stream) == 1:
+            stream = stream[0]
         # If the stream is a dataframe, return the subset matching the key
         if isinstance(stream, pd.DataFrame):
             if not in_stream_key:
@@ -63,15 +69,17 @@ def MProvAgg(in_stream_name,op,out_stream_name,in_stream_key=['index'],out_strea
 
     def inner_function(func):
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(arg):
             #args_repr = [md_key(a, in_stream_key) for a in args]
             #kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
             #signature = ", ".join(args_repr + kwargs_repr)
             #print(f"Calling {func.__name__}({signature})")
-            val = func(*args, **kwargs)
+            val = func(arg)#*args, **kwargs)
+
+            mprov_conn = MProvConnection(mprov_user, mprov_password, mprov_host)
 
             window_ids = []
-            for t in rel_keys(args[0], in_stream_key):
+            for t in rel_keys(arg, in_stream_key):
                 if mprov_conn:
                     window_ids.append(mprov_conn.get_entity_id(in_stream_name, [t[k] for k in in_stream_key]))
                 else:
@@ -82,7 +90,7 @@ def MProvAgg(in_stream_name,op,out_stream_name,in_stream_key=['index'],out_strea
                     out_keys = val.reset_index()[out_stream_key[0]].to_list()
                 else:
                     out_keys = []
-                    print (val.reset_index()[out_stream_key])
+                    print(val.reset_index()[out_stream_key])
                     for tup in val.reset_index()[out_stream_key].iterrows():
                         out_keys.append(tup[1].to_list())
             else:
@@ -91,13 +99,15 @@ def MProvAgg(in_stream_name,op,out_stream_name,in_stream_key=['index'],out_strea
             ts = datetime.now(timezone.utc)
             if mprov_conn:
                 #try:
-                for t in rel_keys(args[0], in_stream_key):
+                for t in rel_keys(arg, in_stream_key):
                     print('Input: %s' %in_stream_name + str([t[k] for k in in_stream_key]))
                     mprov_conn.store_stream_tuple(in_stream_name, [t[k] for k in in_stream_key], blank_tuple)
                 #except:
                 #    pass
+                print (window_ids)
                 mprov_conn.store_windowed_result(out_stream_name, 'w'+str(out_keys), blank_tuple,
                                                  window_ids, op, ts, ts)
+                logging.warning("Output: %s.%s <(%s)- %s", out_stream_name, 'w' + str(out_keys), op, str(window_ids))
             else:
                 logging.warning("Output: %s.%s <(%s)- %s", out_stream_name, 'w'+str(out_keys), op, str(window_ids))
 
@@ -109,11 +119,17 @@ def MProvAgg(in_stream_name,op,out_stream_name,in_stream_key=['index'],out_strea
 def test(n):
     return n.groupby('x').count()
 
+@MProvAgg("ecg", 'test', 'output_ecg',['x'],['x'])
+def testx(n):
+    return n.groupby('x').count()
+
 import pandas as pd
 
 data = pd.DataFrame([{'x':1, 'y': 2}, {'x':3, 'y':4}])
 test(data)
+testx(data)
 
+df = data
 
 ####  [i1] \
 ####  [i2] -- [w_{i1,i2,i3}] -used-> (f) -generates-> [o123]
