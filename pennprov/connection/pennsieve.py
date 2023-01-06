6a8b85f23d7e7beefa8be4a5060c49d8cefced3a
John Frommeyer <johnfr@seas.upenn.edu>
2023-01-06 15:34:55 -0500

    Adding APISession for Pennsieve access.

--- pennprov/connection/pennsieve.py
+++ pennprov/connection/pennsieve.py
@@ -1,12 +1,15 @@
 import boto3
+import jwt
 import requests
 import logging
 import uuid
+import datetime
 
 from pennprov.config import config
 
 
-PENNSIEVE_URL = "https://api.pennsieve.io"
+PENNSIEVE_URL = "https://api.pennsieve.net"
+
 
 class PennsieveClient:
     '''
@@ -17,41 +20,55 @@ class PennsieveClient:
         r = requests.get(f"{PENNSIEVE_URL}/authentication/cognito-config")
         r.raise_for_status()
 
-        cognito_app_client_id = r.json()["tokenPool"]["appClientId"]
-        cognito_region = r.json()["region"]
-
-        print("Connecting to " + cognito_region + ", app " + cognito_app_client_id)
-
-        cognito_idp_client = boto3.client(
+        self.cognito_app_client_id = r.json()["tokenPool"]["appClientId"]
+        self.cognito_region = r.json()["region"]
+        self.cognito_idp_client = boto3.client(
             "cognito-idp",
-            region_name=cognito_region,
+            region_name=self.cognito_region,
             aws_access_key_id="",
             aws_secret_access_key="",
         )
+        self.api_session = None
 
-        # Authenticate to AWS Cognito
-        #
-        # Hack: stub the access and secret keys with empty values so boto does
-        # not look for AWS credentials in the environment. Some versions of boto
-        # fail when they cannot find AWS credentials even though Cognito does
-        # not need creds.
-        login_response = cognito_idp_client.initiate_auth(
-            AuthFlow="USER_PASSWORD_AUTH",
-            AuthParameters={"USERNAME": config.pennsieve.api_key, 
-            "PASSWORD": config.pennsieve.api_secret},
-            ClientId=cognito_app_client_id,
-        )
+    def _ensure_active_api_session(self):
+        if self.api_session is None or self.api_session.is_almost_expired():
+            print("Connecting to " + self.cognito_region + ", app " + self.cognito_app_client_id)
 
-        self.token = login_response["AuthenticationResult"]["AccessToken"]
+            # Authenticate to AWS Cognito
+            #
+            # Hack: stub the access and secret keys with empty values so boto does
+            # not look for AWS credentials in the environment. Some versions of boto
+            # fail when they cannot find AWS credentials even though Cognito does
+            # not need creds.
+            login_response = self.cognito_idp_client.initiate_auth(
+                AuthFlow="USER_PASSWORD_AUTH",
+                AuthParameters={"USERNAME": config.pennsieve.api_key,
+                                "PASSWORD": config.pennsieve.api_secret},
+                ClientId=self.cognito_app_client_id,
+            )
 
-    def _post(self, url, payload, headers):
+            access_token = login_response["AuthenticationResult"]["AccessToken"]
+            id_token = login_response["AuthenticationResult"]["IdToken"]
+
+            claims = jwt.decode(id_token, options={'verify_signature': False})
+            expiration = claims['exp']
+            organization_node_id = claims['custom:organization_node_id']
+            self.api_session = APISession(
+                token=access_token,
+                expiration=expiration,
+                organization_node_id=organization_node_id
+            )
+
+    def _post(self, url, payload):
         '''
         Post message to Pennsieve, with token attached
         '''
+        self._ensure_active_api_session()
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
-            "authorization": "Bearer " + self.token
+            "authorization": "Bearer " + self.api_session.token,
+            "X-ORGANIZATION-ID": self.api_session.organization_node_id
         }
 
         return requests.post(url, json=payload, headers=headers)
@@ -67,7 +84,7 @@ class PennsieveClient:
 
         # Create provenance model
         try:
-            response = self._post(f"{PENNSIEVE_URL}/models/v1/datasets/datasetId/concepts", json=payload)
+            response = self._post(f"{PENNSIEVE_URL}/models/v1/datasets/datasetId/concepts", payload=payload)
 
             payload = [
                 {
@@ -83,7 +100,7 @@ class PennsieveClient:
                 }
             ]
 
-            response = self._post(f"{PENNSIEVE_URL}/models/v1/datasets/datasetId/concepts/url/properties", json=payload)
+            response = self._post(f"{PENNSIEVE_URL}/models/v1/datasets/datasetId/concepts/url/properties", payload=payload)
 
             return response['id']
 
@@ -111,6 +128,64 @@ class PennsieveClient:
 
         return
 
+
+class APISession:
+    """Holds session info for Pennsieve API
+
+    Attributes:
+    ----------
+    token: str
+        The AccessToken returned by Cognito. This token is needed to access Pennsieve API endpoints.
+        Used as the Bearer token in the Authorization header in Pennsieve API requests.
+    organization_node_id: str
+        The node id of the user's organization.
+        Used as the value of the X-ORGANIZATION-ID header in Pennsieve API requests.
+    expiration: datetime.datetime
+        the expiration datetime of the token in UTC
+
+    Methods:
+    --------
+    is_almost_expired()
+        returns True if token is "near" its expiration time, False if not
+
+    """
+    EXPIRATION_MARGIN = datetime.timedelta(minutes=5)
+
+    def __init__(
+            self,
+            token: str,
+            expiration: datetime.datetime | int,
+            organization_node_id: str):
+        """Constructor for APISession
+
+            Parameters:
+            ----------
+            token: str
+                The AccessToken returned by Cognito.
+                Used as the Bearer token in the Authorization header for Pennsieve API requests.
+            expiration: datetime | int
+                The expiration datetime of the token in UTC.
+                If this is a datetime, it should be in UTC.
+                If this is an int, it should be a POSIX timestamp. It will be converted to a datetime
+            organization_node_id: str
+                The node id of the user's organization.
+                Used as the value of the X-ORGANIZATION-ID header in Pennsieve API requests.
+        """
+        self.token = token
+        self.expiration = datetime.datetime.fromtimestamp(expiration,
+                                                          datetime.timezone.utc) if isinstance(expiration,
+                                                                                               int) else expiration
+        self.organization_node_id = organization_node_id
+
+    def is_almost_expired(self) -> bool:
+        now = datetime.datetime.now(datetime.timezone.utc)
+        if now > self.expiration - APISession.EXPIRATION_MARGIN:
+            return True
+        return False
+
+    def __str__(self):
+        return str(self.__dict__)
+
 # Sample code
-c = PennsieveClient()
-c.attach_provenance("N:dataset:0194ebba-8766-4d11-aa62-dda7cc0e28bd", "N:package:68abc46c-fd65-4cdd-8216-22b6eeab0718", "mprov://123-456-789")
+# c = PennsieveClient()
+# c.attach_provenance("N:dataset:0194ebba-8766-4d11-aa62-dda7cc0e28bd", "N:package:68abc46c-fd65-4cdd-8216-22b6eeab0718", "mprov://123-456-789")
